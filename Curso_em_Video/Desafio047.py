41fcd985a7f703b823604d1db5cb190791fd823e
Anderson da Silva Costa <anderson1409@protonmail.com>
2019-04-13 10:33:15 -0300

    Codigo Python Professor Guanabara

---
+++ Curso_em_Video/Desafio047.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+# CURSO EM VÍDEO
+# Curso de Programação em Python
+# Professor: Gustavo Guanabara
+
+print('====== DESAFIO 047 ======')
+# Crie um programa que mostra na tela todos
+# os números pares que estão no intervalo entre 1 e 50
+
+print('Numeros Pares de 1 até 50')
+for i in range(1,51):
+    if (i % 2 == 0):
+        print(i)
